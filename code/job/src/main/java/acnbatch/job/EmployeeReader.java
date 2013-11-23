b4ccfe5cd5bfc35d0c6419700db2b73ed67ff3e4
Arne-Christian Blystad <blyarn10@nith.no>
2013-11-23 16:27:31 +0100

    Started working on EmployeeReader, which will read from a given InputStream as a Excel worksheet, to a Row iterator, which can then be processed by subsequent tasks

--- code/job/src/main/java/acnbatch/job/EmployeeReader.java
+++ code/job/src/main/java/acnbatch/job/EmployeeReader.java
@@ -1,13 +1,45 @@
 package acnbatch.job;
 
 import javax.batch.api.chunk.AbstractItemReader;
+import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 @Named
 public class EmployeeReader extends AbstractItemReader {
 
+
+    private final InputStream inputStream;
+    private Iterator<Row> rowIterator;
+
+    @Inject
+    public EmployeeReader(InputStream workbookInputStream) {
+        this.inputStream = workbookInputStream;
+
+    }
+
     @Override
-    public Object readItem() throws Exception {
-        return null;
+    public void open(Serializable checkpoint) throws Exception {
+        Workbook workbook = WorkbookFactory.create(inputStream);
+
+        rowIterator = workbook.getSheetAt(0).rowIterator();
+        rowIterator.next(); // Skip header
+    }
+
+    @Override
+    public Row readItem() throws Exception {
+        return rowIterator.next();
+    }
+
+    @Override
+    public void close() throws Exception {
+        inputStream.close();
     }
 }
---
+++ code/job/src/test/java/acnbatch/job/EmployeeReaderUnitTest.java
@@ -0,0 +1,23 @@
+package acnbatch.job;
+
+import org.apache.poi.ss.usermodel.Row;
+
+import java.io.InputStream;
+
+/**
+ * Created by blystad on 11/23/13.
+ */
+public class EmployeeReaderUnitTest {
+
+    public void testReadEmployeesFromXlsx() throws Exception {
+        InputStream employeeData = this.getClass().getResourceAsStream("test_data.xlsx");
+
+        EmployeeReader reader = new EmployeeReader(employeeData);
+        reader.open(null);
+
+        Row rowOne = reader.readItem();
+
+        
+    }
+
+}
