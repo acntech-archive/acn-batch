fc76ac9547989f8cdf01245c43e97ce5ed88509b
Teo <onlyteo@live.com>
2013-12-11 19:47:25 +0100

    Added SEQUENCE table to create script. Added dynamic context path to JS WebSocket script

--- code/webapp/src/main/resources/META-INF/create.sql
+++ code/webapp/src/main/resources/META-INF/create.sql
@@ -1,1 +1,17 @@
-CREATE TABLE APP.employee(id BIGINT  PRIMARY KEY NOT NULL,personalnumber VARCHAR (255) NOT NULL,name VARCHAR (255) NOT NULL,email VARCHAR(255) NOT NULL,phone varchar (255) NOT NULL,enterpriseid varchar (255) NOT NULL)
+CREATE TABLE APP.SEQUENCE
+(
+	seq_name		VARCHAR (50)					NOT NULL,
+	seq_count		DECIMAL (15)					NOT NULL
+);
+
+INSERT INTO SEQUENCE (seq_name, seq_count) values ('SEQ_GEN', 0);
+
+CREATE TABLE APP.EMPLOYEE
+(
+	id				BIGINT 			PRIMARY KEY		NOT NULL,
+	personalnumber	VARCHAR (255)					NOT NULL,
+	name			VARCHAR (255)					NOT NULL,
+	email			VARCHAR (255)					NOT NULL,
+	phone			VARCHAR (255)					NOT NULL,
+	enterpriseid	VARCHAR (255)					NOT NULL
+);
--- code/webapp/src/main/webapp/js/notification.js
+++ code/webapp/src/main/webapp/js/notification.js
@@ -1,13 +1,14 @@
 var websocketSession;
 
 function f_onmessage(evt) {
-	websocketMessages = document.getElementById('websocketMessages');
-	websocketMessages.innerHTML = websocketMessages.innerHTML + evt.data + '<br/>';
+	notifications = document.getElementById('notifications');
+	notifications.innerHTML = notifications.innerHTML + evt.data + '<br />';
 }
 
 function open() {
 	if (!websocketSession) {
-		websocketSession = new WebSocket('ws://' + document.location.host + '/webapp/api/notifications');
+		pathArray = document.location.pathname.split('/');
+		websocketSession = new WebSocket('ws://' + document.location.host + '/' + pathArray[1] + '/api/notifications');
 		websocketSession.onmessage = f_onmessage;
 	}
 }
