f2174bfe404d8eae701619a48b35fd6c3899e3fd
Teo <onlyteo@live.com>
2013-12-12 17:54:11 +0100

    Changed create/drop SQLs

--- code/webapp/src/main/resources/META-INF/create.sql
+++ code/webapp/src/main/resources/META-INF/create.sql
@@ -1,17 +1,5 @@
-CREATE TABLE APP.SEQUENCE
-(
-	seq_name		VARCHAR (50)					NOT NULL,
-	seq_count		DECIMAL (15)					NOT NULL
-);
+CREATE TABLE APP.SEQUENCE (seq_name VARCHAR (50) NOT NULL, seq_count DECIMAL (15) NOT NULL)
 
-INSERT INTO SEQUENCE (seq_name, seq_count) values ('SEQ_GEN', 0);
+INSERT INTO SEQUENCE (seq_name, seq_count) values ('SEQ_GEN', 0)
 
-CREATE TABLE APP.EMPLOYEE
-(
-	id				BIGINT 			PRIMARY KEY		NOT NULL,
-	personalnumber	VARCHAR (255)					NOT NULL,
-	name			VARCHAR (255)					NOT NULL,
-	email			VARCHAR (255)					NOT NULL,
-	phone			VARCHAR (255)					NOT NULL,
-	enterpriseid	VARCHAR (255)					NOT NULL
-);
+CREATE TABLE APP.EMPLOYEE (id BIGINT PRIMARY KEY NOT NULL, personalnumber VARCHAR (255) NOT NULL, name VARCHAR (255) NOT NULL, email VARCHAR (255) NOT NULL, phone VARCHAR (255) NOT NULL, enterpriseid VARCHAR (255) NOT NULL)
